52652bfd13d36e026548d23dafde7ca086cd3f6e
Dibash Thapa <dibashthapa55@gmail.com>
2021-02-01 01:16:50 +0545

    init

--- src/commons/config/apiConfig.js
+++ src/commons/config/apiConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  apiPort: process.env.API_PORT || "3000",
+  apiPort: process.env.PORT || '3000',
   secretToken: process.env.SECRET_TOKEN,
   jwtExpiry: process.env.JWT_EXPIRY,
 };
