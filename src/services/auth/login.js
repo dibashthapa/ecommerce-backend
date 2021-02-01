5da67b27f35d7dad12868ea46ed5ae277373d2e2
Dibash Thapa <dibashthapa55@gmail.com>
2021-02-01 16:48:20 +0545

    err message added

--- src/services/auth/login.js
+++ src/services/auth/login.js
@@ -34,7 +34,7 @@ const login = (infra) => async ({ email, password }) => {
     return { token };
   } catch (error) {
     logError(error);
-    throw { error, status: 400 };
+    throw { ...error, status: 400 };
   }
 };
 
