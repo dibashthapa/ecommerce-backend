dd8570e82f7e93b84927a435cf63ed60c1c16482
Dibash Thapa <dibashthapa55@gmail.com>
2021-02-01 16:50:40 +0545

    err message added

--- src/api/http/handlers/auth/login.js
+++ src/api/http/handlers/auth/login.js
@@ -9,14 +9,12 @@ const login = (authService) => async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const { token } = await authService.login({ email, password });
-
     body = { token };
-
     status = 200;
   } catch (error) {
     status = error.status || 500;
     headers = {};
-    body = error.message;
+    body = { ...error.message };
   } finally {
     sendResponse({
       responseWriter,
