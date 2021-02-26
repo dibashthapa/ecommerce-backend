527e936f8dfd3788dc0da0791e78b58441ae135d
Dibash Thapa <dibashthapa55@gmail.com>
2021-02-26 23:16:42 +0545

    feat: Pagination added

--- src/services/products/get.js
+++ src/services/products/get.js
@@ -1,17 +1,18 @@
 const logError = console.error;
 const log = console.log;
-const get = (infra) => async ({ page, size }) => {
+let response = {};
+const get = (infra) => async ({ page }) => {
   try {
     const foundProduct = await infra.findAll();
     const previous = parseInt(page) - 1;
     const start = previous ? previous * 10 : 0;
-    const end = start ? start + 10 : foundProduct.length;
+    const end = start + 10;
     const paginatedProduct = foundProduct.slice(start, end);
-
+    console.log({ start: start, end: end });
+    response['totalPages'] = Math.ceil(foundProduct.length / 10);
     //check if there are two queries i.e. page & size. And throw error
-    if (page && size) throw new Error("A url can't have two queries page & size");
-    if (size) return paginatedProduct.slice(0, size);
-    return paginatedProduct;
+    if (page) response['paginatedProduct'] = paginatedProduct;
+    return response;
   } catch (error) {
     logError(error.message);
     throw { message: error.message, status: 404 };
